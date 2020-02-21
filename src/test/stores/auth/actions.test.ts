d53c7963a623bb01bd920d69d84047b1a607137d
Lemeshonok Kirill <lemeshonok12@gmail.com>
2020-02-21 21:13:53 +0700

    test: add auth login async action test

--- src/test/stores/auth/actions.test.ts
+++ src/test/stores/auth/actions.test.ts
@@ -1,11 +1,61 @@
-import { loginRequestAction } from '../../../stores/auth/actions';
+import { loginRequestAction, loginSuccessAction, loginFailedAction, loginAsync } from '../../../stores/auth/actions';
+import { IUser } from '../../../stores/auth/types';
+import createMockStore from 'redux-mock-store';
+import thunk, { ThunkDispatch } from 'redux-thunk';
+import { AnyAction } from 'redux';
+import { IApplicationState } from '../../../stores/config-reducers';
+
+type DispatchExtension = ThunkDispatch<IApplicationState, void, AnyAction>;
+const mockStore = createMockStore<{}, DispatchExtension>([thunk]);
 
 describe('auth actions', () => {
-  it('should create action for login request', () => {
+  it('login request', () => {
     const expectedAction = {
       type: loginRequestAction.type,
     };
 
     expect(loginRequestAction()).toEqual(expectedAction);
+  });
+
+  it('login success', () => {
+    const user: IUser = {
+      name: 'test',
+    };
+
+    const expectedAction = {
+      type: loginSuccessAction.type,
+      payload: user,
+    };
+
+    expect(loginSuccessAction(user)).toEqual(expectedAction);
+  });
+
+  it('login failed', () => {
+    const error = new Error('test');
+    const expectedAction = {
+      type: loginFailedAction.type,
+      payload: error,
+    };
+
+    expect(loginFailedAction(error)).toEqual(expectedAction);
+  });
+
+  it('login async', async () => {
+    const user: IUser = {
+      name: 'user',
+    };
+    const expectedActions = [
+      { type: loginRequestAction.type },
+      { type: loginSuccessAction.type, payload: user },
+    ];
+
+    const store = mockStore({
+      auth: {},
+    });
+
+    loginAsync('user', 'password');
+    return store.dispatch(loginAsync('user', 'password')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
   });
 });
