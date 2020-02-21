6ae6dc90646d0eaab5d2fc4c5c8c2fde60997976
Lemeshonok Kirill <lemeshonok12@gmail.com>
2020-02-21 20:43:39 +0700

    add login failed action

--- src/stores/auth/actions.ts
+++ src/stores/auth/actions.ts
@@ -6,6 +6,7 @@ import { IUser } from './types';
 
 export const loginRequestAction = createAction<void, '@@auth/loginRequest'>('@@auth/loginRequest');
 export const loginSuccessAction = createAction<IUser, '@@auth/loginSuccess'>('@@auth/loginSuccess');
+export const loginFailedAction = createAction<Error, '@@auth/loginSuccess'>('@@auth/loginSuccess');
 
 export type LoginAsyncThunk = ThunkAction<Promise<void>, IApplicationState, unknown, Action<string>>;
 
